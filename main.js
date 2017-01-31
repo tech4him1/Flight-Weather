a30331e588510764e9be6bddf2a74026acdfdb2b
tech4him <tech4him1@users.noreply.github.com>
2017-01-31 09:58:31 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+var mymap = L.map('mapid');//.setView([51.505, -0.09], 13);
+
+L.tileLayer('https://[a|b|c].tile.openstreetmap.org/${z}/${x}/${y}.png', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+    //maxZoom: 18,
+}).addTo(mymap);
+
 $.get({
     url: "http://www.aviationweather.gov/adds/dataserver_current/httpparam",
     data: {
@@ -8,35 +15,15 @@ $.get({
     },
     dataType : "xml",
 })
-  // Code to run if the request succeeds (is done);
-  // The response is passed to the function
-  .done(function( json ) {
-     $( "<h1>" ).text( json.title ).appendTo( "body" );
-     $( "<div class=\"content\">").html( json.html ).appendTo( "body" );
-  })
-  // Code to run if the request fails; the raw request and
-  // status codes are passed to the function
-  .fail(function( xhr, status, errorThrown ) {
-    alert( "Sorry, there was a problem!" );
-    console.log( "Error: " + errorThrown );
-    console.log( "Status: " + status );
-    console.dir( xhr );
-  })
-  // Code to run regardless of success or failure;
-  .always(function( xhr, status ) {
-    alert( "The request is complete!" );
-  });
-
-
-    var map = new ol.Map({
-      target: 'map',
-      layers: [
-        new ol.layer.Tile({
-          source: new ol.source.OSM()
-        })
-      ],
-      view: new ol.View({
-        center: ol.proj.fromLonLat([37.41, 8.82]),
-        zoom: 4
-      })
-    });
+.done(function( xml ) {
+  var airmets = $( xml ).find( "GAIRMET" );
+  for(let i=0; i < airmets.length; i++) {
+    let area = airmets[i].find( "area" );
+    console.log(area);
+  }
+   L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+  ]).addTo(mymap);
+});
