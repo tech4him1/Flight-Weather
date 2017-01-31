66bda1caf570d0ff1c1bec3ad8597a58a25e7fde
tech4him <tech4him1@users.noreply.github.com>
2017-01-31 10:02:16 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
 var mymap = L.map('mapid');//.setView([51.505, -0.09], 13);
 
 L.tileLayer('https://[a|b|c].tile.openstreetmap.org/${z}/${x}/${y}.png', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-    //maxZoom: 18,
+  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+  //maxZoom: 18,
 }).addTo(mymap);
 
 $.get({
-    url: "//www.aviationweather.gov/adds/dataserver_current/httpparam",
-    data: {
-        dataSource: "gairmets",
-        requestType: "retrieve",
-        format: "xml",
-        flightPath: "KSGU;KDEN;KSHR",
-    },
-    dataType : "xml",
+  url: "//www.aviationweather.gov/adds/dataserver_current/httpparam",
+  data: {
+      dataSource: "gairmets",
+      requestType: "retrieve",
+      format: "xml",
+      flightPath: "KSGU;KDEN;KSHR",
+  },
+  dataType : "xml",
+  xhrFields: {
+    withCredentials: true
+  },
 })
 .done(function( xml ) {
   var airmets = $( xml ).find( "GAIRMET" );
