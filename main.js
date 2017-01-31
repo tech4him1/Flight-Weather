383cc976666a679940992613de0a9a8f7f7038cb
Caleb <inchristalway-tech4him@yahoo.com>
2017-01-31 16:41:42 -0700

    Update.

--- main.js
+++ main.js
@@ -8,13 +8,13 @@ const styles = {
   "MT_OBSC": { color: "darkpink" },
 }
 
+const disabledTypes = ["FZLVL", "IFR", "MT_OBSC"];
 
-
-var mymap = L.map('mapid').setView([39.8282, -98.5795], 4);
+var mainMap = L.map('mainMap').setView([39.8282, -98.5795], 4);
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(mymap);
+}).addTo(mainMap);
 
 $.get({
   url: "https://adds-forwarder.herokuapp.com/dataserver_current/httpparam",
@@ -23,7 +23,7 @@ $.get({
       requestType: "retrieve",
       format: "xml",
       flightPath: "0;KDEN;KSHR",
-      hoursBeforeNow: 2,
+      hoursBeforeNow: 3,
   },
   dataType : "xml",
   crossDomain: true,
@@ -31,13 +31,28 @@ $.get({
 .done(function( xml ) {
   console.log(xml);
   $( xml ).find( "GAIRMET" ).each(function(){
-    let shape = [];
+    let type = $( this ).children( "hazard" ).attr( "type" );
+    if ( disabledTypes.includes(type) ) return;
+    let outline = [];
     $( this ).find( "area point" ).each(function(){
         let longitude = $(this).children("longitude")[0].innerHTML;
         let latitude = $(this).children("latitude")[0].innerHTML;
-        shape.push([Number(latitude), Number(longitude)]);
+        outline.push([Number(latitude), Number(longitude)]);
     });
-    let type = $( this ).children( "hazard" ).attr( "type" );
-    L.polygon(shape, styles[type]).addTo(mymap);
+    // Remove duplicate beginning and ending elements.
+    outline = removeBeginEndDuplicates(outline);
+    let geometry_type =  $( this ).children( "geometry_type" )[0].innerHTML;
+    if (geometry_type === "AREA") {
+      L.polygon(outline, styles[type])
+        .addTo(mainMap)
+        .bindTooltip(type);
+    } else if (geometry_type === "LINE") {
+      L.polyline(outline, styles[type])
+        .addTo(mainMap);
+    }
   });
 });
+
+function removeBeginEndDuplicates(polygon) {
+  return polygon;//FILLER
+}
