aa333ba2be7b484baba48b9d35d3f3035ff076a9
Caleb <inchristalway-tech4him@yahoo.com>
2017-01-31 20:54:17 -0700

    Updates.

--- experimental/main.js
+++ experimental/main.js
@@ -1,7 +1,7 @@
 const styles = {
   "TURB-HI": { color: "darkorange" },
   "TURB-LO": { color: "darkred" },
-  "LLWS": { color: "darkpurple" },
+  "LLWS": { color: "brown" },
   "ICE": { color: "darkblue" },
   "FZLVL": { color: "blue" },
   "IFR": { color: "darkpurple" },
@@ -9,6 +9,16 @@ const styles = {
 }
 
 const disabledTypes = ["FZLVL", "IFR", "MT_OBSC"];
+
+const flightData = {
+  path: "KSGU;KDEN;KSHR",
+  time: new Date('2017-02-02T14:25:00Z'),
+}
+const searchDistance = 100;
+
+var timeUntilFlight = flightData.time - Date.now();
+
+const HOURS = 60*60*1000;
 
 var mainMap = L.map('mainMap').setView([39.8282, -98.5795], 4);
 
@@ -22,37 +32,68 @@ $.get({
       dataSource: "gairmets",
       requestType: "retrieve",
       format: "xml",
-      flightPath: "0;KDEN;KSHR",
-      hoursBeforeNow: 3,
+      flightPath: searchDistance + ";" + flightData.path,
+      hoursBeforeNow: 3, // We just need the latest forecasts.
   },
   dataType : "xml",
   crossDomain: true,
 })
 .done(function( xml ) {
-  console.log(xml);
-  $( xml ).find( "GAIRMET" ).each(function(){
-    let type = $( this ).children( "hazard" ).attr( "type" );
-    if ( disabledTypes.includes(type) ) return;
-    let outline = [];
-    $( this ).find( "area point" ).each(function(){
-        let longitude = $(this).children("longitude")[0].innerHTML;
-        let latitude = $(this).children("latitude")[0].innerHTML;
-        outline.push([Number(latitude), Number(longitude)]);
-    });
-    // Remove duplicate beginning and ending elements.
-    outline = removeBeginEndDuplicates(outline);
-    let geometry_type =  $( this ).children( "geometry_type" )[0].innerHTML;
-    if (geometry_type === "AREA") {
-      L.polygon(outline, styles[type])
-        .addTo(mainMap)
-        .bindTooltip(type);
-    } else if (geometry_type === "LINE") {
-      L.polyline(outline, styles[type])
-        .addTo(mainMap);
-    }
-  });
+  $( xml )
+    .find( "GAIRMET" )
+    .filter(function(){
+      // Only use AIRMETs that are 1.5 hours or less from the flight time. AIRMETs are issued every three hours for three hour periods in advance, so 1.5 hours is half of 3.
+      let airmet_time = new Date( $( this ).children( "expire_time" ).text() );
+      if (Math.abs(airmet_time - flightData.time) < 1.5*HOURS) {
+        return true;
+      }
+    })
+    .each(function(){
+      var mapItem;
+
+      // Get the hazard type.
+      let type = $( this ).children( "hazard" ).attr( "type" );
+
+      // Remove unwanted types.
+      if ( disabledTypes.includes(type) ) return;
+
+      // Make an outline of the polygon from the points.
+      let outline = [];
+      $( this ).find( "area point" ).each(function(){
+          let longitude = $(this).children("longitude").text();
+          let latitude = $(this).children("latitude").text();
+          outline.push([Number(latitude), Number(longitude)]);
+        });
+
+        // Remove duplicate beginning and ending elements.
+        outline = removeBeginEndDuplicates(outline);
+
+        // Is it a polygon or a line?
+        let geometry_type =  $( this ).children( "geometry_type" ).text();
+        if (geometry_type === "AREA") {
+          mapItem = L.polygon(outline, styles[type])
+          .bindPopup(type);
+        } else if (geometry_type === "LINE") {
+          mapItem = L.polyline(outline, styles[type]);
+        }
+
+        // Add the item to the layer.
+        addToLayer(mapItem, type);
+
+      });
+
+  L.control.layers(null, layers).addTo(mainMap);
 });
 
 function removeBeginEndDuplicates(polygon) {
   return polygon;//FILLER
+}
+
+var layers = {};
+function addToLayer(mapItem, type) {
+  if (layers[type] === undefined) {
+    layers[type] = L.layerGroup();
+    mainMap.addLayer(layers[type]);
+  }
+  layers[type].addLayer(mapItem);
 }
--- index.md
+++ index.md
@@ -3,3 +3,6 @@ layout: page
 title: Main
 ---
 ![](https://www.aviationweather.gov/adds/data/progs/hpc_48_fcst.gif)
+
+<div id="mainMap" style="height: 750px;"></div>
+<script src="experimental/main.js"></script>
