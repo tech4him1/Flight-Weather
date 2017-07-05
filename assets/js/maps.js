a6fc32c31f096afe3ba112584b5ee7794f59e474
werckerbot <pleasemailus@wercker.com>
2017-07-05 02:09:59 +0000

    [ci skip] deploy from greenkeeper[bot]

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -2,41 +2,38 @@ var Maps =
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
-
+/******/
 /******/ 	// The require function
 /******/ 	function __webpack_require__(moduleId) {
-
+/******/
 /******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId])
+/******/ 		if(installedModules[moduleId]) {
 /******/ 			return installedModules[moduleId].exports;
-
+/******/ 		}
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = installedModules[moduleId] = {
 /******/ 			i: moduleId,
 /******/ 			l: false,
 /******/ 			exports: {}
 /******/ 		};
-
+/******/
 /******/ 		// Execute the module function
 /******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-
+/******/
 /******/ 		// Flag the module as loaded
 /******/ 		module.l = true;
-
+/******/
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
 /******/ 	}
-
-
+/******/
+/******/
 /******/ 	// expose the modules object (__webpack_modules__)
 /******/ 	__webpack_require__.m = modules;
-
+/******/
 /******/ 	// expose the module cache
 /******/ 	__webpack_require__.c = installedModules;
-
-/******/ 	// identity function for calling harmony imports with the correct context
-/******/ 	__webpack_require__.i = function(value) { return value; };
-
+/******/
 /******/ 	// define getter function for harmony exports
 /******/ 	__webpack_require__.d = function(exports, name, getter) {
 /******/ 		if(!__webpack_require__.o(exports, name)) {
@@ -47,7 +44,7 @@ var Maps =
 /******/ 			});
 /******/ 		}
 /******/ 	};
-
+/******/
 /******/ 	// getDefaultExport function for compatibility with non-harmony modules
 /******/ 	__webpack_require__.n = function(module) {
 /******/ 		var getter = module && module.__esModule ?
@@ -56,15 +53,15 @@ var Maps =
 /******/ 		__webpack_require__.d(getter, 'a', getter);
 /******/ 		return getter;
 /******/ 	};
-
+/******/
 /******/ 	// Object.prototype.hasOwnProperty.call
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
-
+/******/
 /******/ 	// __webpack_public_path__
 /******/ 	__webpack_require__.p = "";
-
+/******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 5);
+/******/ 	return __webpack_require__(__webpack_require__.s = 2);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -82,8 +79,8 @@ module.exports = {
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-/* harmony export (immutable) */ __webpack_exports__["a"] = removeBeginEndDuplicates;
-/* harmony export (immutable) */ __webpack_exports__["b"] = addToLayer;
+/* harmony export (immutable) */ __webpack_exports__["b"] = removeBeginEndDuplicates;
+/* harmony export (immutable) */ __webpack_exports__["a"] = addToLayer;
 //import L from 'leaflet';
 //import $ from 'jQuery';
 
@@ -105,16 +102,44 @@ function addToLayer(layers, mapItem, type) {
 
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__airmets__ = __webpack_require__(3);
+/* harmony export (immutable) */ __webpack_exports__["createMap"] = createMap;
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__map_handlers_index__ = __webpack_require__(3);
+
+
+function createMap(mapType, mapID) {
+  var map = L.map(mapID).setView([39.8282, -98.5795], 4);
+
+  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+  
+  __WEBPACK_IMPORTED_MODULE_0__map_handlers_index__[mapType](function(layers){
+    for (let layer in layers) {
+      if (layers.hasOwnProperty(layer)) {
+        map.addLayer(layers[layer]);
+      }
+    }
+    L.control.layers(null, layers).addTo(map);
+  });
+}
+
+
+/***/ }),
+/* 3 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__airmets__ = __webpack_require__(4);
 /* harmony reexport (binding) */ __webpack_require__.d(__webpack_exports__, "airmets", function() { return __WEBPACK_IMPORTED_MODULE_0__airmets__["a"]; });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__sigmets__ = __webpack_require__(4);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__sigmets__ = __webpack_require__(5);
 /* harmony reexport (binding) */ __webpack_require__.d(__webpack_exports__, "sigmets", function() { return __WEBPACK_IMPORTED_MODULE_1__sigmets__["a"]; });
 
 
 
 
 /***/ }),
-/* 3 */
+/* 4 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
@@ -143,7 +168,7 @@ const HOURS = 60*60*1000;
 // A storage space for groups of layers.
 var layers = {};
 
-/* harmony default export */ __webpack_exports__["a"] = function(callback) {
+/* harmony default export */ __webpack_exports__["a"] = (function(callback) {
   $.get({
     url: "https://adds-forwarder.herokuapp.com/dataserver_current/httpparam",
     data: {
@@ -184,7 +209,7 @@ var layers = {};
           });
 
           // Remove duplicate beginning and ending elements.
-          outline = __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__helpers_js__["a" /* removeBeginEndDuplicates */])(outline);
+          outline = __WEBPACK_IMPORTED_MODULE_1__helpers_js__["b" /* removeBeginEndDuplicates */](outline);
 
           // Is it a polygon or a line?
           let geometry_type =  $( this ).children( "geometry_type" ).text();
@@ -196,15 +221,15 @@ var layers = {};
           }
 
           // Add the item to the appropriate layer.
-          __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__helpers_js__["b" /* addToLayer */])(layers, mapItem, type);
+          __WEBPACK_IMPORTED_MODULE_1__helpers_js__["a" /* addToLayer */](layers, mapItem, type);
         });
       callback(layers);
     });
-};
+});
 
 
 /***/ }),
-/* 4 */
+/* 5 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
@@ -229,7 +254,7 @@ const styles = {
 // A storage space for groups of layers.
 var layers = {};
 
-/* harmony default export */ __webpack_exports__["a"] = function(callback) {
+/* harmony default export */ __webpack_exports__["a"] = (function(callback) {
   $.get({
     url: "https://adds-forwarder.herokuapp.com/dataserver_current/httpparam",
     data: {
@@ -268,46 +293,18 @@ var layers = {};
           });
 
           // Remove duplicate beginning and ending elements.
-          outline = __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__helpers_js__["a" /* removeBeginEndDuplicates */])(outline);
+          outline = __WEBPACK_IMPORTED_MODULE_1__helpers_js__["b" /* removeBeginEndDuplicates */](outline);
 
           // SIGMETs are always polygons.
           mapItem = L.polygon(outline, styles[type])
             .bindPopup(type);
 
           // Add the item to the appropriate layer.
-          __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__helpers_js__["b" /* addToLayer */])(layers, mapItem, type);
+          __WEBPACK_IMPORTED_MODULE_1__helpers_js__["a" /* addToLayer */](layers, mapItem, type);
         });
       callback(layers);
     });
-};
-
-
-/***/ }),
-/* 5 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__map_handlers_index__ = __webpack_require__(2);
-/* harmony export (immutable) */ __webpack_exports__["createMap"] = createMap;
-
-
-function createMap(mapType, mapID) {
-  var map = L.map(mapID).setView([39.8282, -98.5795], 4);
-
-  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(map);
-  
-  __WEBPACK_IMPORTED_MODULE_0__map_handlers_index__[mapType](function(layers){
-    for (let layer in layers) {
-      if (layers.hasOwnProperty(layer)) {
-        map.addLayer(layers[layer]);
-      }
-    }
-    L.control.layers(null, layers).addTo(map);
-  });
-}
+});
 
 
 /***/ })
